890cb45f922b36919e8c22bfba1a66e0e987875d
Mikhail Treushnikov <www.mor.ry@mail.ru>
2019-12-18 22:10:57 +0300

    Третий коммит

--- src/task5.cpp
+++ src/task5.cpp
@@ -6,24 +6,10 @@ int getSumInt(int arr[], int N)
 	int a = 0, j = N-1,sum = 0;
 	for (int i = 0; i < N; i++)
 	{
-		a = rand() % 2;
-		switch (a)
-		{
-		case 0:
-			arr[i] = -1*(1+rand() % 9);
-			break;
-		case 1:
-			arr[i] =1+rand() % 9;
-			break;
-		}
-	}
-
-	for (int i = 0; i < N; i++)
-	{
 		printf("%d ", arr[i]);
 	}
 	putchar('\n');
-	while (a != j)
+	while (a < j)
 	{
 		if ((arr[a] < 0) && (arr[j] > 0))
 		{
@@ -40,6 +26,11 @@ int getSumInt(int arr[], int N)
 		else if ((arr[a] > 0) && (arr[j] >0))
 		{
 			a++;
+		}
+		else
+		{
+			a++;
+			j--;
 		}
 	}
 	return sum;
--- src/task6.cpp
+++ src/task6.cpp
@@ -3,21 +3,8 @@
 #include <string.h>
 int getSumMaxMin(int arr[], int N)
 {
-	int min = 0, max = 0, sum = 0,ran = 0;
+	int min = 0, max = 0, sum = 0;
 	int* p = 0;
-	for (int i = 0; i < N; i++)
-	{
-		ran = rand() % 2;
-		switch (ran)
-		{
-		case 0:
-			arr[i] = -1 * (1 + rand() % 9);
-			break;
-		case 1:
-			arr[i] = 1 + rand() % 9;
-			break;
-		}
-	}
 
 	for (int i = 0; i < N; i++)
 	{
@@ -31,7 +18,6 @@ int getSumMaxMin(int arr[], int N)
 		if (max < arr[i])
 			max = arr[i];
 	}
-	printf("max - %d\nmin - %d\n", max, min);
 	int var = 0;
 	for (int a = 0; a < N; a++)
 	{
