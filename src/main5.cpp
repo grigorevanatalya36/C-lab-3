cc62c03ca2041c872b4977b4e4e76fac67cccef3
Mikhail Treushnikov <www.mor.ry@mail.ru>
2019-12-18 22:16:17 +0300

    Забыл закинуть main 5,6

--- src/main5.cpp
+++ src/main5.cpp
@@ -7,10 +7,24 @@
 
 int main()
 {
+	int a = 0;
 	time_t now;
 	time(&now);
 	srand(now);
-	int arr[N] = { 0 };
+	int arr[N] = {0};
+	for (int i = 0; i < N; i++)
+	{
+		a = rand() % 2;
+		switch (a)
+		{
+		case 0:
+			arr[i] = -1 * (1 + rand() % 9);
+			break;
+		case 1:
+			arr[i] = 1 + rand() % 9;
+			break;
+		}
+	}
 	printf("%d\n", getSumInt(arr,N));
 	return 0;
 }
